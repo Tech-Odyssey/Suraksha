c8fcc3165eb035889ef341dd558d72aa9f61bd94
Sameer Kumar <mesameerkumar@gmail.com>
2021-11-07 19:00:38 +0530

    added images and some ui changes

--- app/src/main/java/com/example/shuraksha/homeact.java
+++ app/src/main/java/com/example/shuraksha/homeact.java
@@ -3,6 +3,7 @@ package com.example.shuraksha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class homeact extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class homeact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeact);
+    }
+
+    public void gotohelpline(View view) {
+    }
+
+    public void gotomap(View view) {
     }
 }
