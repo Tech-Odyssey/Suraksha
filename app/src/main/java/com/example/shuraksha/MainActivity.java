14abd729597dabf39715453111bdfeeff9b93e34
Arnavt14 <77206185+Arnavt14@users.noreply.github.com>
2021-11-15 15:58:24 +0530

    title bar hide(removed)

--- app/src/main/java/com/example/shuraksha/MainActivity.java
+++ app/src/main/java/com/example/shuraksha/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
-        getSupportActionBar().hide(); // hide the title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
