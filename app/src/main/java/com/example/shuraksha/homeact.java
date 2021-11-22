40920abde995628037b5e8799206316cec9c53ee
Arnavt14 <77206185+Arnavt14@users.noreply.github.com>
2021-11-22 06:54:26 +0530

    messages shifted to home act
    Messages have been shifted to homeact class, and sos button has been linked for sending the mssgs when pressed

--- app/src/main/java/com/example/shuraksha/homeact.java
+++ app/src/main/java/com/example/shuraksha/homeact.java
@@ -2,11 +2,18 @@ package com.example.shuraksha;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -18,11 +25,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 
+
 public class homeact extends AppCompatActivity {
     Button mlogout;
     Button mmessages;
     ImageView mSos;
-    Button mssgs;
+    EditText mnumber;
+    EditText mmessageforSOS;
+    final int SEND_SMS_REQUEST_CODE =1;
     GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +41,42 @@ public class homeact extends AppCompatActivity {
         mSos = findViewById(R.id.sos_img);
         mlogout = findViewById(R.id.logout);
         mmessages = findViewById(R.id.MSSGS);
-        mSos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Its working", Toast.LENGTH_SHORT).show();
-            }
-        });
+        mnumber = findViewById(R.id.editTextPhone);
+        mmessageforSOS = findViewById(R.id.messagehome);
+
         mmessages.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent2 = new Intent(homeact.this,messages.class);
                 startActivity(intent2);
+            }
+        });
+
+        // for messages in home page via edit text|| trying to connect the SOS button so that it acts as the send button for sms
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            mSos.setEnabled(true);
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},SEND_SMS_REQUEST_CODE);
+        }
+        mSos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String no=mnumber.getText().toString();
+                String msg=mmessageforSOS.getText().toString();
+
+                msg = "This is a SOS message all available units please respond";
+
+                //Getting intent and PendingIntent instance
+                Intent intent=new Intent(getApplicationContext(),messages.class);
+                PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+
+                //Get the SmsManager instance and call the sendTextMessage method to send message
+                SmsManager sms=SmsManager.getDefault();
+                sms.sendTextMessage(no, null, msg, pi,null);
+
+                Toast.makeText(getApplicationContext(), "Message Sent successfully!",
+                        Toast.LENGTH_LONG).show();
             }
         });
         mlogout.setOnClickListener(new View.OnClickListener() {
@@ -77,26 +111,11 @@ public class homeact extends AppCompatActivity {
         startActivity(intent2);
 
     }
-
-    /*private void signOut() {
-        mGoogleSignInClient.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Intent intent2 = new Intent(homeact.this,login.class);
-                        startActivity(intent2);
-                    }
-                });
+    public boolean checkPermission(String Permission){
+        int check = ContextCompat.checkSelfPermission(this,Permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
     }
-    private void revokeAccess() {
-        mGoogleSignInClient.revokeAccess()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-    }*/
+
 
 
 }
